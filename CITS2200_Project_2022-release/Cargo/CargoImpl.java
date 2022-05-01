6e0e47911efb8f697096f1fcde877e4f4b345677
Aaaleexx <hungminh29999@gmail.com>
2022-05-01 09:13:24 +0700

    Initial commit

---
+++ CITS2200_Project_2022-release/Cargo/CargoImpl.java
@@ -0,0 +1,14 @@
+// Full Name (StudentNum), Full Name (StudentNum)
+
+/**
+ * An implementation of the Cargo problem from the 2022 CITS2200 Project
+ */
+public class CargoImpl implements Cargo {
+  /**
+   * {@inheritdoc}
+   */
+  public int[] departureMasses(int stops, Query[] queries) {
+    // TODO: Implement your solution
+    return null;
+  }
+}
---
+++ CITS2200_Project_2022-release/Fines/FinesImpl.java
@@ -0,0 +1,14 @@
+// Full Name (StudentNum), Full Name (StudentNum)
+
+/**
+ * An implementation of the Fines problem from the 2022 CITS2200 Project
+ */
+public class FinesImpl implements Fines {
+  /**
+   * {@inheritdoc}
+   */
+  public int countFines(int[] priorities) {
+    // TODO: Implement your solution
+    return -1;
+  }
+}
